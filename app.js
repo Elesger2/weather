c12f36faabffa05e110ce9cde0bd90959ad13018
Elesger Musayev <elesgermusayev860@gmail.com>
2023-09-08 10:52:00 +0000

    app

--- app.js
+++ app.js
@@ -1,52 +1,33 @@
 "use strict";
 $(document).ready(function () {
-
-    $("#me").click(function (e) {
-        e.preventDefault();
-        function myel(myclass = "") {
-            if (!$.trim($("#inputme").val()) == '') {
-                let vall = $("#inputme").val()
-                    , newH3 = $("<h3> </h3>")
-                    , newDiv = $('<div class="line" > </div>')
-                    , bigDiv = $(`<div ${myclass} > </div>`)
-                $(newH3).append(vall);
-                $(newDiv).append(newH3);
-                $(bigDiv).append(newDiv);
-                return bigDiv
+    $(document).on('keypress', function (e) {
+        if (e.which == 13) {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=18bf37475451448f442e7f86714cd476&units=metric`
+            async function myweather() {
+                let responce = await fetch(url)
+                let data = await responce.json()
+                $("#city").html(data.name);
+                $("#temp").html(data.main.temp);
+                $("#feels").html(data.main.feels_like);
+                $("#myWeather").val("");
+                console.log(data)
             }
-            else {
-                return
-            }
+            myweather()
         }
-        $("#messageyou").append(myel())
-        $("#messageme").append(myel('class="end"'))
-        $("#inputme").val("")
-        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)
-        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
     });
 
-    $("#you").click(function (e) {
-        e.preventDefault()
-        function myel(myclass = "") {
-            if (!$.trim($("#inputyou").val()) == '') {
-                let vall = $("#inputyou").val()
-                    , newH3 = $('<h3> </h3>')
-                    , newDiv = $('<div class="line" > </div>')
-                    , bigDiv = $(`<div ${myclass} > </div>`)
-                $(newH3).append(vall)
-                $(newDiv).append(newH3)
-                $(bigDiv).append(newDiv)
-                return bigDiv
-            }
-            else {
-                return
-            }
+    $("#btn").click(function (e) {
+        e.preventDefault();
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=18bf37475451448f442e7f86714cd476&units=metric`
+        async function myweather() {
+            let responce = await fetch(url)
+            let data = await responce.json()
+            $("#city").html(data.name);
+            $("#temp").html(data.main.temp);
+            $("#feels").html(data.main.feels_like);
+            $("#myWeather").val("");
+            console.log(data)
         }
-        $("#messageme").append(myel())
-        $("#messageyou").append(myel('class="end"'))
-        $("#inputyou").val("")
-        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)
-        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
-    })
-
+        myweather()
+    });
 });
